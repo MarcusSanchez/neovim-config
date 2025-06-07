a3bea5f51772c02f93e287c0613e04020aef33e0
= <marcussanchez031@gmail.com>
2025-06-07 15:04:41 -0400

    push from windows

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -35,3 +35,12 @@ vim.api.nvim_create_autocmd("LspAttach", {
     })
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.expandtab = true
+  end,
+})
